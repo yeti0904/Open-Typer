0aaa221f15b7862ac5727ba0ba04b9b69d413732
adazem009 <adazem009@users.noreply.github.com>
2022-01-29 20:27:37 +0000

    deploy: b923fd6b4588d4da00ba55c9ded9ddd85b8a955f

--- classkeyboardWidget.js
+++ classkeyboardWidget.js
@@ -7,7 +7,7 @@ var classkeyboardWidget =
     [ "fingerHand", "classkeyboardWidget.html#a13cedfbe131d6fa5ccc3e437ea694b20", null ],
     [ "highlightKey", "classkeyboardWidget.html#a6618df4de1c15adca1e312714ff711ae", null ],
     [ "keyFinger", "classkeyboardWidget.html#a64808e6cc8df9a440770ca14b7add951", null ],
-    [ "loadLayout", "classkeyboardWidget.html#a4d48587f4501f9934978e2fba4bd33d8", null ],
+    [ "loadLayout", "classkeyboardWidget.html#a89e1f9931b347cb71ce5d553d19b70bd", null ],
     [ "nextRow", "classkeyboardWidget.html#ac57098beebc362b0981db8fe82b4ac57", null ],
     [ "registerKey", "classkeyboardWidget.html#ae7ab31a9b366bfa013300fb78bbb5f23", null ],
     [ "resetKeyColor", "classkeyboardWidget.html#aee8d43defa9db5129a48086563f72b65", null ],
