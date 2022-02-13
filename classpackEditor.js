7fd66a788ba691d3be2cc4467085f8ba978e076e
adazem009 <adazem009@users.noreply.github.com>
2022-02-13 09:50:42 +0000

    deploy: ec895faf6bb1ecf6463befde640d233459a0993c

--- classpackEditor.js
+++ classpackEditor.js
@@ -2,7 +2,6 @@ var classpackEditor =
 [
     [ "packEditor", "classpackEditor.html#a2531c78a0bd8650d5395147566ed50c9", null ],
     [ "~packEditor", "classpackEditor.html#acc6fa330df5ca29c78538929eeabe382", null ],
-    [ "close", "classpackEditor.html#a7a0f4ec6830a8aaae728deab8762edea", null ],
     [ "closeAll", "classpackEditor.html#ad1160ecc7f6d1396455a20b804bfc952", null ],
     [ "closeEvent", "classpackEditor.html#aa403472b118aef534f8093106316ce05", null ],
     [ "closeTab", "classpackEditor.html#a8029fd89ffc29702456698439e7a1d39", null ],
@@ -13,5 +12,6 @@ var classpackEditor =
     [ "openFile", "classpackEditor.html#a517c6e4e5c95ad37b0d22f60b0a41fc2", null ],
     [ "openPrebuilt", "classpackEditor.html#ae4e25dd318728fc46546e7116e751a91", null ],
     [ "setFileName", "classpackEditor.html#acab56991754277171feb8adebd317130", null ],
-    [ "setNewFile", "classpackEditor.html#ad40ff79b94794d3df4929ed1970bd0ff", null ]
+    [ "setNewFile", "classpackEditor.html#ad40ff79b94794d3df4929ed1970bd0ff", null ],
+    [ "tabChanged", "classpackEditor.html#a21cb0d01d5588aff614bf4d7e89e8c11", null ]
 ];
--- search/functions_11.js
+++ search/functions_11.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['testconnection_0',['testConnection',['../classconnectionOptions.html#a122737d3de84bd61b2daabdbf0cf0d8c',1,'connectionOptions']]],
-  ['themechanged_1',['themeChanged',['../classcustomizationOptions.html#a91aecd4bea4879c9eb40f1c91c08b2ea',1,'customizationOptions::themeChanged()'],['../classoptionsWindow.html#a469fc19d55f3b6ca1467540f40c0f7a1',1,'optionsWindow::themeChanged()']]],
-  ['timedialog_2',['timeDialog',['../classtimeDialog.html#afee89fd18b8ade41b001e436dc73f522',1,'timeDialog']]],
-  ['togglekeyboard_3',['toggleKeyboard',['../classkeyboardWidget.html#a2f270d1d090d383e02509947774abceb',1,'keyboardWidget']]]
+  ['tabchanged_0',['tabChanged',['../classpackEditor.html#a21cb0d01d5588aff614bf4d7e89e8c11',1,'packEditor']]],
+  ['testconnection_1',['testConnection',['../classconnectionOptions.html#a122737d3de84bd61b2daabdbf0cf0d8c',1,'connectionOptions']]],
+  ['themechanged_2',['themeChanged',['../classcustomizationOptions.html#a91aecd4bea4879c9eb40f1c91c08b2ea',1,'customizationOptions::themeChanged()'],['../classoptionsWindow.html#a469fc19d55f3b6ca1467540f40c0f7a1',1,'optionsWindow::themeChanged()']]],
+  ['timedialog_3',['timeDialog',['../classtimeDialog.html#afee89fd18b8ade41b001e436dc73f522',1,'timeDialog']]],
+  ['togglekeyboard_4',['toggleKeyboard',['../classkeyboardWidget.html#a2f270d1d090d383e02509947774abceb',1,'keyboardWidget']]]
 ];
