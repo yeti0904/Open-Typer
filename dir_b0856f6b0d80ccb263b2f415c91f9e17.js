a9e49ca20da9b69f26effd8a81a79ed6cf3f9aca
adazem009 <adazem009@users.noreply.github.com>
2022-03-15 20:05:34 +0000

    deploy: e0b0c106bcc927bffdf5324c21ab5b192d894c01

--- dir_b0856f6b0d80ccb263b2f415c91f9e17.js
+++ dir_b0856f6b0d80ccb263b2f415c91f9e17.js
@@ -5,7 +5,6 @@ var dir_b0856f6b0d80ccb263b2f415c91f9e17 =
     [ "packEditor", "dir_d684c3475f66f131a133112539f1805c.html", "dir_d684c3475f66f131a133112539f1805c" ],
     [ "updater", "dir_7468157ebcb76345a67412f7bb97ed29.html", "dir_7468157ebcb76345a67412f7bb97ed29" ],
     [ "widgets", "dir_99f4b16b17a360d9840a4176588db3c6.html", "dir_99f4b16b17a360d9840a4176588db3c6" ],
-    [ "global.h", "global_8h_source.html", null ],
     [ "initialsetup.h", "initialsetup_8h_source.html", null ],
     [ "levelsummary.h", "levelsummary_8h_source.html", null ],
     [ "opentyper.h", "opentyper_8h_source.html", null ],
