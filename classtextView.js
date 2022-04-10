3ec130c1a1f386136c2490af36dd032fd498ee93
adazem009 <adazem009@users.noreply.github.com>
2022-04-10 13:47:15 +0000

    deploy: 6dd4eb974224729247c2e9006180107a79e4f3b7

--- classtextView.js
+++ classtextView.js
@@ -3,6 +3,7 @@ var classtextView =
     [ "textView", "classtextView.html#ad6aaddb0b664f37e2794b57cbe31e81c", null ],
     [ "~textView", "classtextView.html#aae65bbcd6440ddf2261f783c18aad0a6", null ],
     [ "changeEvent", "classtextView.html#a195c0b35129f798662b90dc13d79a31d", null ],
+    [ "mouseMoveEvent", "classtextView.html#a30a8c4751cb9d7bfd29a5aaafab937b8", null ],
     [ "setHorizontalAdjust", "classtextView.html#a17845e2af54e6b6e3ded66981125f108", null ],
     [ "setVerticalAdjust", "classtextView.html#a8436c65cc75d5ede073ac7b8d90d743c", null ],
     [ "updateWidgetSize", "classtextView.html#a5ee657378078136e98e01796f9d5e946", null ],
--- search/all_b.js
+++ search/all_b.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['mainsettingslocation_0',['mainSettingsLocation',['../classfileUtils.html#a3c18f5df4c77a63e197dab52ebaaaaac',1,'fileUtils']]],
   ['maxfontsize_1',['maxFontSize',['../classthemeEngine.html#a3a36600c19ac5a4562db01d118dbde35',1,'themeEngine']]],
-  ['monitorclient_2',['monitorClient',['../classmonitorClient.html',1,'monitorClient'],['../classmonitorClient.html#a6fedfbb816b9ce8cd17080f97d11be00',1,'monitorClient::monitorClient()']]]
+  ['monitorclient_2',['monitorClient',['../classmonitorClient.html',1,'monitorClient'],['../classmonitorClient.html#a6fedfbb816b9ce8cd17080f97d11be00',1,'monitorClient::monitorClient()']]],
+  ['mousemoveevent_3',['mouseMoveEvent',['../classtextView.html#a30a8c4751cb9d7bfd29a5aaafab937b8',1,'textView']]]
 ];
