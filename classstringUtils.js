f278374b30a38dc70aa19f571ee6a7ffd747a3a5
adazem009 <adazem009@users.noreply.github.com>
2022-05-22 18:41:00 +0000

    deploy: 07cd43b925fd787852f0700a7839025c1d8a0def

--- classstringUtils.js
+++ classstringUtils.js
@@ -6,6 +6,7 @@ var classstringUtils =
     [ "lcsLen", "classstringUtils.html#a79faa227c39594f727c1008f29e1cfe3", null ],
     [ "longestCommonSubsequence", "classstringUtils.html#a341fb091d92a8afbf2e6fcd52d85c132", null ],
     [ "longestCommonSubsequence", "classstringUtils.html#a71a472149dd1b3f6eeca6ae90a274a02", null ],
+    [ "splitWordsByPunct", "classstringUtils.html#a19f0a6330da206c790741ccdc36b877d", null ],
     [ "word", "classstringUtils.html#a4abcd8458b8b50e9d6679e6f03788dc6", null ],
     [ "wordAt", "classstringUtils.html#a0b9afdf922dcc4f6fbd6b331b7a65453", null ],
     [ "wordCount", "classstringUtils.html#a84f6e3df1c51c8b65fe60fbd031e01f2", null ]
